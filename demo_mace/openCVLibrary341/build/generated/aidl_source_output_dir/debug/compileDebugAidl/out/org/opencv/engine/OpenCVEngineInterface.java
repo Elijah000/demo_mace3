70df3b49e90682dfe24d59a42227d85f6a5411ce
旻昌 <school021195@gmail.com>
2019-08-18 14:44:39 +0800

    測試改了一個字

--- demo_mace/openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ demo_mace/openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\user\\Documents\\GitHub\\PoseEstimationForMobile\\android_demo\\demo_mace\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\user\\Desktop\\0818\\demo_mace\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
